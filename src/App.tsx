0f7f4a3023b1fe06e15fdfa3322e999a8f16daca
muhammadtalha242 <m.talha2244@gmail.com>
2022-12-18 16:18:17 +0500

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Col, Row } from "antd";
 
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <Row>
+      <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+        Col
+      </Col>
+      <Col xs={20} sm={16} md={12} lg={8} xl={4}>
+        Col
+      </Col>
+      <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+        Col
+      </Col>
+    </Row>
+  );
 }
 
 export default App;
