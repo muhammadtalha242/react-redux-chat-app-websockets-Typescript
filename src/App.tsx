245e83e306bbfa91d643c537458d36223300badf
muhammadtalha242 <m.talha2244@gmail.com>
2022-12-19 17:16:41 +0500

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import "./App.css";
 import { Col, Row } from "antd";
 
+import SideBar from "./components/sidebar";
+import Display from "./components/display";
+import Input from "./components/input";
+import { DisplayInputRowContainer } from "./container";
+
 function App() {
   return (
     <Row>
-      <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-        Col
+      <Col span={8}>
+        <SideBar />
       </Col>
-      <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-        Col
-      </Col>
-      <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-        Col
-      </Col>
+      <DisplayInputRowContainer span={16}>
+        <Display />
+        <Input />
+      </DisplayInputRowContainer>
+     
     </Row>
   );
 }
--- src/container.tsx
+++ src/container.tsx
@@ -1,3 +1,11 @@
+import { Col } from "antd";
 import styled from "styled-components";
 
 export const APPContainer = styled.div``;
+
+export const DisplayInputRowContainer = styled(Col)`
+  flex-direction: column;
+  display: flex;
+  justify-content: space-between;
+  height: 100vh;
+`;
